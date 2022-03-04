1c60b166f3e8bc6657e53a359d86ea74262d6052
Sergio Radigales <sergioradigales@gmail.com>
2022-03-04 20:52:24 +0100

    fix5

--- src/components/phoneView/phoneView.js
+++ src/components/phoneView/phoneView.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import Footer from "../footer/footer";
+
 import './phoneView.css'
 
 const PhoneView = () => {
